d1338b8a95ff513217db5afb283834673385d4b0
niccolospa <niccolo.spagnuolo@gmail.com>
2018-04-30 15:42:19 +0200

    checkbox added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 import taskdb
 app = Flask(__name__)
 
@@ -10,6 +10,22 @@ def hello_world():
 def index():
     tasks = taskdb.update_tasks()
     return render_template("index.html", taskmap = tasks)
+
+@app.route('/insert',methods=['POST'])
+def insert():
+    new = request.form['new']
+    urgent = request.form.get('urgent')
+    if urgent == "on":
+        urgent = 1
+    else:
+        urgent = 0
+    taskdb.insert_task(new, urgent)
+    return redirect(url_for('index'))
+
+@app.route('/delete/<task_id>')
+def delete(task_id):
+    taskdb.remove_task(task_id)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
--- taskdb.py
+++ taskdb.py
@@ -11,3 +11,23 @@ def update_tasks():
     connection.close()
     tasks = dict((x, y) for x, y in result)
     return tasks
+
+def insert_task(new, u):
+    sql = "insert into todo (task, urgent) value (%s, %s)"
+    connection = pymysql.connect(user="root", password="", host="localhost", database="task")
+    cursor = connection.cursor()
+    cursor.execute(sql, (new,u))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
+
+def remove_task(id):
+    sql = "delete from todo where id = (%s)"
+    connection = pymysql.connect(user="root", password="", host="localhost", database="task")
+    cursor = connection.cursor()
+    cursor.execute(sql, (id,))
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return
